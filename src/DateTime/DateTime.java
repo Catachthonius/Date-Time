6952347b2abb2dfdd092952c5da60be174e160ff
Antonio <catachthonius01@gmail.com>
2023-06-20 01:18:47 +0200

    First commit

---
+++ src/DateTime/DateTime.java
@@ -0,0 +1,27 @@
+import java.time.LocalDate;
+
+public class DateTime {
+    public static void main(String[] args) {
+
+        //Using LocaleDate or another Date class calculate and print:
+        //How many days are left till the end of the month
+        //How many days are left till the end of the year, using the length() method from java.time.Year.of()
+
+        LocalDate currentDate = LocalDate.now();
+
+        //Calculating number of days in current month and current day of the month:
+        int daysInOneMonth = currentDate.lengthOfMonth();
+        int currentDay = currentDate.getDayOfMonth();
+
+        //Calculating how many days are left until the end of the month:
+        int daysUntilEndOfTheMonth = daysInOneMonth - currentDay;
+        System.out.println("Days left until the end of the month: " + daysUntilEndOfTheMonth);
+
+        //Calculating number of days in current year and number of days left until the end of the year:
+        int daysInOneYear = currentDate.getDayOfYear();
+        int daysUntilEndOfTheYear = daysInOneYear - currentDay;
+
+        System.out.println("Days left until the end of the year: " + daysUntilEndOfTheYear);
+
+    }
+}
